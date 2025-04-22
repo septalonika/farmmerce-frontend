e8199cf3af905f0fbb9faa8e4ce8a10c899767ec
septalonika <andikabeatbox@gmail.com>
2025-04-22 15:42:33 +0700

    init sample components

---
+++ src/app/home/page.tsx
@@ -0,0 +1,13 @@
+import Home from "@/components/organisms/home";
+import { $sampleStores } from "../stores/sampleStores";
+import { useStore } from "@nanostores/react";
+
+const HomePage = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default HomePage;
