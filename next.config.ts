3a2c6c5078244786a7bb9d6b863723f0c2629fa5
septalonika <andikabeatbox@gmail.com>
2025-04-22 21:45:40 +0700

    define : create colour pallete

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // #ffffff base color
+  // #c0c0c0
+  // #808080
+  // #404040
+  // #000000 font color
 };
 
 export default nextConfig;
