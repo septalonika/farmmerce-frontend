54e093f5d155f3bb929748280b76014f9d5222bb
septalonika <andikabeatbox@gmail.com>
2025-04-22 22:40:43 +0700

    init: header & footer

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -1,27 +1,6 @@
 "use client";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import Link from "next/link";
-import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarTrigger,
-} from "@/components/ui/menubar";
-
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu";
-import { Button } from "@/components/ui/button";
 
 const jakartaSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -29,39 +8,11 @@ const jakartaSans = Plus_Jakarta_Sans({
 
 const Header = () => {
   return (
-    <header className="flex h-[6dvh] items-center">
-      <Button
-        className={`${jakartaSans.className} bg-transparent text-black antialiased duration-95 hover:scale-95 hover:bg-transparent`}
-      >
-        Farmmerce.
-      </Button>
-      <Menubar>
-        <MenubarMenu>
-          <MenubarTrigger className="border-none border-white">
-            File
-          </MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem>
-              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-            </MenubarItem>
-            <MenubarItem>New Window</MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem>Share</MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem>Print</MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-      </Menubar>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+    <header className="flex h-[6dvh] w-full border-b-2">
+      <div className="mx-[8dvw] flex w-full items-center justify-between">
+        <div className="flex"></div>
+        <div className="flex"></div>
+      </div>
     </header>
   );
 };
