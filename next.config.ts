f43e248b0e7d337f71142d5a0418ac7ab2ff639f
septalonika <andikabeatbox@gmail.com>
2025-04-22 15:36:07 +0700

    remove peerDependencies

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,"peerDependencies": {
-    "react": "^17.0.0 || ^18.0.0 || ^19.0.0",
-    "react-dom": "^17.0.0 || ^18.0.0 || ^19.0.0"
-  },
 };
 
 export default nextConfig;
