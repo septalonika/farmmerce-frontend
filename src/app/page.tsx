5c0c9c4cd134b8708bc797835659beb68fafcfc2
septalonika <andikabeatbox@gmail.com>
2025-04-22 15:41:54 +0700

    using paging foldering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,11 @@
-'use client'
+import Home from "@/components/organisms/home";
 
-// export default function Home() {
-//   return (
-//     <div>
-//       test
-//     </div>
-//   );
-// }
-
-import { useStore } from '@nanostores/react'
-import { $users } from '@/app/stores/user'
-import Rating from '@mui/material/Rating';
-
-
-const Users = () => {
-  const admins = useStore($users)
+const HomePage = () => {
   return (
-    <div className='flex h-screen items-center justify-center'>
-      <ul>
-          {admins.map(user => <div>
-            <li key={user.id}>
-              {user.id}
-              {user.name}
-              {user.email}
-              {/* <Button variant="contained">Hello world</Button>; */}
-              <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
-            </li>
-          </div>)}
-      </ul>
+    <div>
+      <Home />
     </div>
-  )
-}
+  );
+};
 
-export default Users;
+export default HomePage;
