24d231cca7c1021193e598dda610524c7a7e8759
septalonika <andikabeatbox@gmail.com>
2025-04-23 09:49:55 +0700

    init: adding header page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Home from "@/components/organisms/home";
 
 const HomePage = () => {
-  return <div className="min-h-[82dvh]">test</div>;
+  return (
+    <div className="min-h-[82dvh]">
+      <Home />
+    </div>
+  );
 };
 
 export default HomePage;
