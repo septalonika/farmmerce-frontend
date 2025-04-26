f0a64a2764d493cdfb8a3bd6c437d0f714560805
Muhammad Syifa Surya Saputra <syifamuhammad3139@gmail.com>
2025-04-26 19:42:59 +0700

    chore(next.config) : remove rewrites configuration

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async rewrites() {
-    const authPages = [
-      "login",
-      "register",
-      "forgot-password",
-      "reset-password",
-    ]; // Auth Pages to be moved to /auth directory
-
-    const pages = ["about", "contact", "privacy-policy", "terms-of-service"]; // Pages in this array will be route to the /pages directory, this array just an example you can add more pages as needed
-
-    const rewrites = [
-      ...authPages.map((page) => {
-        return {
-          source: `/${page}`,
-          destination: `/auth/${page}`,
-        };
-      }),
-      ...pages.map((page) => {
-        return {
-          source: `/${page}`,
-          destination: `/pages/${page}`,
-        };
-      }),
-    ];
-
-    return rewrites; // result of this code, for examples : Instead of writing /auth/login, you can just write /login and it will be redirected to /auth/login
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
