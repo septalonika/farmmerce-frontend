4ed54bfda68e2933a677b2f176c25cb3b9e6d4ee
Muhammad Syifa Surya Saputra <syifamuhammad3139@gmail.com>
2025-04-26 17:04:28 +0700

    feat : add rewrites for auth & pages in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,32 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    const authPages = [
+      "login",
+      "register",
+      "forgot-password",
+      "reset-password",
+    ];
+    const pages = ["about", "contact", "privacy-policy", "terms-of-service"];
+
+    const rewrites = [
+      ...authPages.map((page) => {
+        return {
+          source: `/${page}`,
+          destination: `/auth/${page}`,
+        };
+      }),
+      ...pages.map((page) => {
+        return {
+          source: `/${page}`,
+          destination: `/pages/${page}`,
+        };
+      }),
+    ];
+
+    return rewrites;
+  },
+};
 
 export default nextConfig;
